8cd59133e855f16f6dc7ab39d88ef4eb76425aff
piratemeow <imranrahman543@gmail.com>
2024-11-24 08:24:46 +0600

    Two endpoints for destinations created. Database connection and real data fetching peding

--- server/src/main/java/com/example/travelApp/controllers/DestinationController.java
+++ server/src/main/java/com/example/travelApp/controllers/DestinationController.java
@@ -1,4 +1,29 @@
 package com.example.travelApp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class DestinationController {
+
+    @GetMapping("/destinations/{id}")
+    public Map<String, String> showDestinationById(@PathVariable Integer id) {
+        String message = "Successfully Received the request with id " + id;
+        Map<String, String> response = new HashMap<>();
+        response.put("Message", message);
+
+        return response;
+    }
+
+    @GetMapping("/destinations/search")
+    public Map<String ,String> searchDestination(){
+        String message = "Successfully Received the request";
+        Map<String, String> response = new HashMap<>();
+        response.put("Message", message);
+        return response;
+    }
 }
